0dedd914abf2a2267a6e053c0829bf91a04d402b
masterbg94 <nmilinkovic3@gmail.com>
2022-02-16 14:39:26 +0100

    Content encoding Gzip

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); //protection
+  // somewhere in your initialization file
+  app.use(compression());
 
   const config = new DocumentBuilder()
     .setTitle('La Pista API Routes')
